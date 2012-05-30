869dd00c4d67c2888ebbf4b0a1bf4ad35f66381c
Thanasis Polychronakis <thanpolas@gmail.com>
2012-05-30 19:42:28 +0300

    Load latest jQuery from local

--- application/views/ss/loadScripts.php
+++ application/views/ss/loadScripts.php
@@ -1,11 +1,11 @@
 <?php
 
-if (DEVEL && !$this->config->item('load_compiled_js')): 
+if (DEVEL && !$this->config->item('load_compiled_js')):
 	// if in devel mode, load jquery from our files, and load up
 	// the JS engine one by one...
 	// if in production load the compiled js and jquery from google's CDN
 ?>
-<script src="/jsc/jquery-1.5.1.min.js"></script>
+<script src="/js/third-party/jquery-1.7.1.js"></script>
 <script type="text/javascript" src="/js/closure-library/closure/goog/base.js" charset="utf-8"></script>
 <script type="text/javascript" src="/js/deps.js" charset="utf-8"></script>
 <script type="text/javascript" src="/js/init.js" charset="utf-8"></script>
@@ -15,7 +15,7 @@ if (DEVEL && !$this->config->item('load_compiled_js')):
 <?php else: ?>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="/jsc/v/main_<?php echo PRODCOUNTER; ?>.js" charset="utf-8"></script>
-<?php  endif; 
+<?php  endif;
 
 // now echo our interface with our JS engine
 
