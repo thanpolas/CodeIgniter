228cbf5e38de71aead6682bf4b6fd48175b187a4
Thanasis Polychronakis <thanpolas@gmail.com>
2012-06-02 23:25:37 +0300

    Moved FB element on top of script loads

--- application/views/ss/footer.php
+++ application/views/ss/footer.php
@@ -1,8 +1,8 @@
+<div id="fb-root"></div>
 <?php
 	if(!$doNotLoadScripts) {
-		$this->load->view('ss/loadScripts', array());		
+		$this->load->view('ss/loadScripts', array());
 	}
 ?>
-<div id="fb-root"></div>
 </body>
 </html>
